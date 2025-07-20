9c2ce367fbb060beb203e455244f06b20602470d
ayoubelsheikh1 <ayoubelsheikh1@gmail.com>
2025-07-19 18:40:46 -0700

    Add files via upload

---
+++ masark-mvp/pages/employer.tsx
@@ -0,0 +1,8 @@
+export default function Employer() {
+  return (
+    <div className="p-4 text-right">
+      <h2 className="text-xl font-semibold">لوحة الشركة</h2>
+      <p>هنا يمكنك نشر الوظائف وإدارة المتقدمين.</p>
+    </div>
+  );
+}
---
+++ masark-mvp/pages/jobs.tsx
@@ -0,0 +1,8 @@
+export default function Jobs() {
+  return (
+    <div className="p-4 text-right">
+      <h2 className="text-xl font-semibold">الوظائف المتاحة</h2>
+      <p>قائمة الوظائف ستظهر هنا.</p>
+    </div>
+  );
+}
