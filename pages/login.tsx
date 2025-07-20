67fe4fe275dcc4ef05567f9f642735d0119ee6e6
ayoubelsheikh1 <ayoubelsheikh1@gmail.com>
2025-07-19 19:09:28 -0700

    Add files via upload

---
+++ pages/login.tsx
@@ -0,0 +1,8 @@
+export default function Login() {
+  return (
+    <div className="p-4 text-right">
+      <h2 className="text-xl font-semibold">تسجيل الدخول</h2>
+      <p>أدخل رقم الهاتف أو البريد الإلكتروني لتسجيل الدخول.</p>
+    </div>
+  );
+}
