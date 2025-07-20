96de2bba6a4d93a536d996133cbaf37dfa9b5203
ayoubelsheikh1 <ayoubelsheikh1@gmail.com>
2025-07-19 19:03:03 -0700

    Delete pages directory

--- pages/admin.tsx
+++
@@ -1,8 +0,0 @@
-export default function Admin() {
-  return (
-    <div className="p-4 text-right">
-      <h2 className="text-xl font-semibold">لوحة الإدارة</h2>
-      <p>إحصائيات ومراجعة الوظائف والشركات.</p>
-    </div>
-  );
-}
--- pages/index.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div className="text-right p-4">
-      <h1 className="text-2xl font-bold">منصة مسارك للتوظيف</h1>
-      <p className="mt-2">ابدأ رحلتك المهنية الآن</p>
-    </div>
-  );
-}
