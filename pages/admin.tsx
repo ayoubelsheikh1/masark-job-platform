332d79fda12a7fb78e72fbd392b0a1c71a8a3a6d
ayoubelsheikh1 <ayoubelsheikh1@gmail.com>
2025-07-19 18:43:01 -0700

    Add files via upload

---
+++ pages/admin.tsx
@@ -0,0 +1,8 @@
+export default function Admin() {
+  return (
+    <div className="p-4 text-right">
+      <h2 className="text-xl font-semibold">لوحة الإدارة</h2>
+      <p>إحصائيات ومراجعة الوظائف والشركات.</p>
+    </div>
+  );
+}
