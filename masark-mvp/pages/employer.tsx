347d2f0abd556e8dcf1914b1549ec413867d0ea4
ayoubelsheikh1 <ayoubelsheikh1@gmail.com>
2025-07-19 18:42:41 -0700

    Delete masark-mvp directory

--- masark-mvp/pages/employer.tsx
+++
@@ -1,8 +0,0 @@
-export default function Employer() {
-  return (
-    <div className="p-4 text-right">
-      <h2 className="text-xl font-semibold">لوحة الشركة</h2>
-      <p>هنا يمكنك نشر الوظائف وإدارة المتقدمين.</p>
-    </div>
-  );
-}
--- masark-mvp/pages/profile.tsx
+++
@@ -1,8 +0,0 @@
-export default function Profile() {
-  return (
-    <div className="p-4 text-right">
-      <h2 className="text-xl font-semibold">الملف الشخصي</h2>
-      <p>هنا يمكنك تعديل بياناتك وإنشاء السيرة الذاتية.</p>
-    </div>
-  );
-}
